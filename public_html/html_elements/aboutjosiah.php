ab6ecc6a7161ff100fa1e5b90588688f5b7f0dce
psalv <psalvato@uwo.ca>
2017-06-03 14:34:29 -0400

    Basic about content added.

--- public_html/html_elements/aboutjosiah.php
+++ public_html/html_elements/aboutjosiah.php
@@ -1,36 +1,60 @@
 <div class="landerAbout row josiahAbout">
 
-    <div class="col-sm-6">
+    <div class="col-lg-6">
         <div class="row">
             <h2 class="inset title">Josiah "Jo-Jo" Scott Morash</h2>
-            <h3 class="inset title">Remembering him always.</h3>
+            <h4 class="inset title">Remembering him always.</h4>
         </div>
 
-        <div class="row">
+        <div class="row inset title aboutInfo">
+            <p>
+                Josiah was born on July 28th, 2009 at St. Joseph's Hospital in London, Ontario.
+            </p>
 
-            <div class="col-sm-8">
-                <!--            <img src="./assets/img/About/footprint.jpg" class="img-responsive">-->
+            <p>
+                Early into the pregnancy Josiah showed that he was going to be a very special baby.
+                At 18 weeks Josiah was diagnosed with a very rare genetic disorder known as Trisomy 13 or Patau's syndrome.
+            </p>
 
-            </div>
+            <p>
+                Because of the genetic disorder, Trisomy 13, Josiah was given only a 3% chance of surviving the pregnancy.
+                Beating incredible odds, Josiah grew close to full term and was born alive. Josiah was our Miracle Baby.
+            </p>
 
-            <div class="col-sm-4 aboutWords">
+            <p>
+                Due to the many complications of Trisomy 13, Josiah also died this very same day - his birth day.
+            </p><br>
+
+            <p class="topBottomBorder">
+                Josiah's life was a Great Blessing to our family.  He brought more faith, hope and love to our lives than
+                we could have ever imagined.
+            </p><br>
+
+            <p>
+                Please join us as we continue with Josiah's legacy - a precious little red-headed
+                boy whose spirit was filled with courage.  Let us always remember Josiah as we reach out to help other families
+                in need of neonatal and paediatric intensive care.
+            </p>
 
 
-
-            </div>
 
         </div>
 
     </div>
 
-    <div class="col-sm-6">
+<!--    <div class="col-sm-6 text-center">-->
+    <div class="col-lg-6">
         <img src="./assets/img/About/footprint.jpg" class="img-responsive imgCenter">
+<!--        <img src="./assets/img/About/footprint.jpg" class="imgCenter">-->
     </div>
-
-
 
 </div>
 
+<div class="row text-center space">
+    <h4>
+        "Every good and perfect gift comes from above." James 1:17
+    </h4>
+</div>
 
 <!--
 
