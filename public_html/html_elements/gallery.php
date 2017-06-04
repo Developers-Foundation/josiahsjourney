4322ddb600c469763d7e50369b0ad9067342a0f5
psalv <psalvato@uwo.ca>
2017-06-03 21:54:41 -0400

    Formatting sponsors for mobile.

--- public_html/html_elements/gallery.php
+++ public_html/html_elements/gallery.php
@@ -5,81 +5,122 @@
             <div class="col-lg-12 text-center">
                 <div class="gallery">
 
+                    <div class="row text-center">
+                        <h4>Platinum</h4>
+                    </div>
+
                     <div class="row">
-                        <div class="col-md-3">
-                            <img src="./assets/img/sponsors/platinum/bobfm.png" class="img-centered">
+                        <div class="col-xs-6">
+                            <div class="col-md-6">
+                                <img src="./assets/img/sponsors/platinum/bobfm.png" class="img-centered img-responsive">
+                            </div>
+                            <div class="col-md-6">
+                                <img src="./assets/img/sponsors/platinum/canadian_tire.png" class="img-centered img-responsive">
+                            </div>
                         </div>
-                        <div class="col-md-3">
-                            <img src="./assets/img/sponsors/platinum/canadian_tire.png" class="img-centered">
+
+                        <div class="col-xs-6">
+                            <div class="col-md-6">
+                                <img src="./assets/img/sponsors/platinum/datametrix.png" class="img-centered img-responsive">
+                            </div>
+                            <div class="col-md-6">
+                                <img src="./assets/img/sponsors/platinum/scotiobank.png" class="img-centered img-responsive">
+                            </div>
                         </div>
-                        <div class="col-md-3">
-                            <img src="./assets/img/sponsors/platinum/datametrix.png" class="img-centered">
+                    </div>
+
+                    <div class="row text-center">
+                        <h4>Gold</h4>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-xs-6">
+                            <div class="col-md-6">
+                                <img src="./assets/img/sponsors/gold/ashleygilbert.png" class="img-centered img-responsive">
+                            </div>
+                            <div class="col-md-6">
+                                <img src="./assets/img/sponsors/gold/bbqfeast.png" class="img-centered img-responsive">
+                            </div>
                         </div>
-                        <div class="col-md-3">
-                            <img src="./assets/img/sponsors/platinum/scotiobank.png" class="img-centered">
+
+                        <div class="col-xs-6">
+                            <div class="col-md-6">
+                                <img src="./assets/img/sponsors/gold/boosterjuice.png" class="img-centered img-responsive">
+                            </div>
+                            <div class="col-md-6">
+                                <img src="./assets/img/sponsors/gold/shoppers.png" class="img-centered img-responsive">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row text-center">
+                        <h4>Silver</h4>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-xs-6">
+                            <div class="col-md-6">
+                                <img src="./assets/img/sponsors/silver/carajohnston.png" class="img-centered img-responsive">
+                            </div>
+                            <div class="col-md-6">
+                                <img src="./assets/img/sponsors/silver/creativetype.png" class="img-centered img-responsive">
+                            </div>
+                        </div>
+
+                        <div class="col-xs-6">
+                            <div class="col-md-6">
+                                <img src="./assets/img/sponsors/silver/scotts.png" class="img-centered img-responsive">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row text-center">
+                        <h4>Bronze</h4>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-xs-6">
+                            <div class="col-md-6">
+                                <img src="./assets/img/sponsors/bronze/button.png" class="img-centered img-responsive">
+                            </div>
+                            <div class="col-md-6">
+                                <img src="./assets/img/sponsors/bronze/gibson.png" class="img-centered img-responsive">
+                            </div>
+                        </div>
+
+                        <div class="col-xs-6">
+                            <div class="col-md-6">
+                                <img src="./assets/img/sponsors/bronze/haye.png" class="img-centered img-responsive">
+                            </div>
+                            <div class="col-md-6">
+                                <img src="./assets/img/sponsors/bronze/rootham.png" class="img-centered img-responsive">
+                            </div>
                         </div>
                     </div>
 
                     <div class="row">
-                        <div class="col-md-3">
-                            <img src="./assets/img/sponsors/gold/ashleygilbert.png" class="img-centered">
+                        <div class="col-xs-6">
+                            <div class="col-md-6">
+                                <img src="./assets/img/sponsors/bronze/reblyn.png" class="img-centered img-responsive">
+                            </div>
+                            <div class="col-md-6">
+                                <img src="./assets/img/sponsors/bronze/runningroom.png" class="img-centered img-responsive">
+                            </div>
                         </div>
-                        <div class="col-md-3">
-                            <img src="./assets/img/sponsors/gold/bbqfeast.png" class="img-centered">
-                        </div>
-                        <div class="col-md-3">
-                            <img src="./assets/img/sponsors/gold/boosterjuice.png" class="img-centered">
-                        </div>
-                        <div class="col-md-3">
-                            <img src="./assets/img/sponsors/gold/shoppers.png" class="img-centered">
+
+                        <div class="col-xs-6">
+                            <div class="col-md-6">
+                                <img src="./assets/img/sponsors/bronze/starbucks.png" class="img-centered img-responsive">
+                            </div>
+                            <div class="col-md-6">
+                                <img src="./assets/img/sponsors/bronze/superstore.png" class="img-centered img-responsive">
+                            </div>
                         </div>
                     </div>
 
                     <div class="row">
-                        <div class="col-md-3">
-                            <img src="./assets/img/sponsors/silver/carajohnston.png" class="img-centered">
-                        </div>
-                        <div class="col-md-3">
-                            <img src="./assets/img/sponsors/silver/creativetype.png" class="img-centered">
-                        </div>
-                        <div class="col-md-3">
-                            <img src="./assets/img/sponsors/silver/scotts.png" class="img-centered">
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-md-3">
-                            <img src="./assets/img/sponsors/bronze/button.png" class="img-centered">
-                        </div>
-                        <div class="col-md-3">
-                            <img src="./assets/img/sponsors/bronze/gibson.png" class="img-centered">
-                        </div>
-                        <div class="col-md-3">
-                            <img src="./assets/img/sponsors/bronze/haye.png" class="img-centered">
-                        </div>
-                        <div class="col-md-3">
-                            <img src="./assets/img/sponsors/bronze/rootham.png" class="img-centered">
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-md-3">
-                            <img src="./assets/img/sponsors/bronze/reblyn.png" class="img-centered">
-                        </div>
-                        <div class="col-md-3">
-                            <img src="./assets/img/sponsors/bronze/runningroom.png" class="img-centered">
-                        </div>
-                        <div class="col-md-3">
-                            <img src="./assets/img/sponsors/bronze/starbucks.png" class="img-centered">
-                        </div>
-                        <div class="col-md-3">
-                            <img src="./assets/img/sponsors/bronze/superstore.png" class="img-centered">
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-md-3">
-                            <img src="./assets/img/sponsors/bronze/vermeer.png" class="img-centered">
+                        <div class="col-xs-6">
+                            <img src="./assets/img/sponsors/bronze/vermeer.png" class="img-centered img-responsive">
                         </div>
                     </div>
 
